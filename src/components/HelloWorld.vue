1ef74382db180def38c7dbf2f6f023c23fef6814
Sai Main <saimain.primary@gmail.com>
2023-11-14 11:00:09 +0630

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,25 +5,11 @@
 
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify 4</h1>
+      <h1 class="text-h2 font-weight-bold">PWA Test 1</h1>
 
       <div class="py-14" />
 
       <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon icon="mdi-view-dashboard" size="large" start />
-
-            Components
-          </v-btn>
-        </v-col>
-
         <v-col cols="auto">
           <v-btn
             color="primary"
